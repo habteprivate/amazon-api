1d27f1c7f31be0f8dfb464419e1a8a6cc10fb22d
Habtamu <73044474+habteprivate@users.noreply.github.com>
2024-09-26 22:15:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const app = express();
+app.use(cors({ origin: true }));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Success!",
+  });
+});
+
+app.post("/payment/create", async (req, res) => {
+  const total = parseInt(req.query.total);
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+    res.status(201).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(403).json({
+      message: "total must be greater than 0",
+    });
+  }
+});
+
+app.listen(8061, (error) => {
+  if (error) throw error;
+  console.log(
+    "Amazon Server is up and Running on PORT: 8061, http://localhost:8061"
+  );
+});
+
+// https://dashboard.stripe.com/test/payments
